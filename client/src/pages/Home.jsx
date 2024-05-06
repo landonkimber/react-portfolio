dcec276726828c33a542bfc2e6a466d894741a50
landonkimber <landonkimber33@gmail.com>
2024-05-06 14:52:10 -0600

    Made font tweak on home page. Added Molle.tff file to assets

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,12 @@ const Home = () => {
     <Link to="/about">
       <div className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-orange-500 to-rose-600 ">
         <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4 text-white">Welcome!</h1>
+          <h1
+            className="text-9xl font-bold mb-4 text-white"
+            style={{ fontFamily: "Molle, cursive" }}
+          >
+            Welcome!
+          </h1>
           <h2 className="text-2xl text-white">Click to get started!</h2>
         </div>
         {/* <div
